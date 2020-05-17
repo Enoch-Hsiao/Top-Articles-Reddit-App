f0a5cb15969c0810b8b775c46c0cdbda1ed9a5eb
Enoch Hsiao <enochhsiao@hotmail.com>
2020-05-17 12:14:26 -0400

    Updates

---
+++ precache-manifest.fdbf2934dba1164b5a1ecaad48bf4bc1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0d694e13e92053b657af3a4e0aa080",
+    "url": "/top-articles-reddit-app/index.html"
+  },
+  {
+    "revision": "44d7959f1b4c2afc7a6e",
+    "url": "/top-articles-reddit-app/static/css/main.eb6f4366.chunk.css"
+  },
+  {
+    "revision": "fbad0120b25499738fa0",
+    "url": "/top-articles-reddit-app/static/js/2.875b893f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/top-articles-reddit-app/static/js/2.875b893f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d7959f1b4c2afc7a6e",
+    "url": "/top-articles-reddit-app/static/js/main.751cff0e.chunk.js"
+  },
+  {
+    "revision": "7519d3b8c0ffa4ec5831",
+    "url": "/top-articles-reddit-app/static/js/runtime-main.b29a992c.js"
+  },
+  {
+    "revision": "6e9063b0e75a183c07b5d8e9fe828aad",
+    "url": "/top-articles-reddit-app/static/media/gaming.6e9063b0.png"
+  },
+  {
+    "revision": "b6b390019bb747bf6574f78cc43acdc0",
+    "url": "/top-articles-reddit-app/static/media/music.b6b39001.png"
+  },
+  {
+    "revision": "5adc2985b571b4a16f4e3128b543a45d",
+    "url": "/top-articles-reddit-app/static/media/news.5adc2985.png"
+  },
+  {
+    "revision": "a52d3fed9553f20deb9d225ae5722e18",
+    "url": "/top-articles-reddit-app/static/media/photos.a52d3fed.png"
+  },
+  {
+    "revision": "2faa8b641cd4075d292df3e58461a9c7",
+    "url": "/top-articles-reddit-app/static/media/popular.2faa8b64.png"
+  },
+  {
+    "revision": "f7237ab82463d0cfc14a7cd6aabec228",
+    "url": "/top-articles-reddit-app/static/media/science.f7237ab8.png"
+  },
+  {
+    "revision": "79e7975c5e2878a54daa6212758e71fc",
+    "url": "/top-articles-reddit-app/static/media/sports.79e7975c.png"
+  }
+]);
